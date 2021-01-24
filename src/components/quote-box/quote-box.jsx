3c9c75f668aac1c7a55a94b8f46dabac06942355
Yusuf İpek <yusufipek@protonmail.com>
2021-01-24 15:58:44 +0300

    added quote-box component

---
+++ src/components/quote-box/quote-box.jsx
@@ -0,0 +1,15 @@
+import "./quote-box-styles.scss";
+
+const QuoteBox = () => {
+  return (
+    <div className="wrapper">
+      <div id="quote-box">
+        <h3 id="text">Deneme</h3>
+        <p id="author">Deneme</p>
+        {/* new quote button */}
+      </div>
+    </div>
+  );
+};
+
+export default QuoteBox;
